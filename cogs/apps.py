7535baede32c6d2a7f0009a62f29decd48b65ebd
Kevin Vu <kevinvu@Brightfinds-MacBook-Pro.local>
2019-05-12 20:59:14 -0500

    apps change grammer

--- cogs/apps.py
+++ cogs/apps.py
@@ -16,9 +16,9 @@ class Apps(commands.Cog):
     @app.command()
     async def edit(self, ctx, num, *, editString=""):
         # The Bot
-        botUser = self.bot.get_user(502967681956446209)
+        botUser = self.bot.get_user(566024681824452619)
         # App Logs channel 
-        channel = self.bot.get_channel(566024681824452619) 
+        channel = self.bot.get_channel(388591318814949376) 
 
         msgFound = False
         async with channel.typing():
@@ -48,7 +48,7 @@ class Apps(commands.Cog):
             if botMsg.id == r.message.id:
                 sameMessage = True
 
-            return ((str(r.emoji) == '✅' or str(r.emoji) == '🚫') and sameMessage and u.name != "Bot Friend")
+            return ((str(r.emoji) == '✅' or str(r.emoji) == '❌' or str(r.emoji) == '🚼') and sameMessage and u.name != "Bot Friend")
 
         # appchannel
         channel = self.bot.get_channel(388591318814949376)
@@ -57,7 +57,8 @@ class Apps(commands.Cog):
             botMsg = await channel.send(embed=msg.embeds[0])
             await msg.delete()
             await botMsg.add_reaction('✅')
-            await botMsg.add_reaction('🚫')
+            await botMsg.add_reaction('❌')
+            await botMsg.add_reaction('🚼')
 
             mReaction, mUser = await self.bot.wait_for("reaction_add", check=msgCheck)
             appDict = botMsg.embeds[0].to_dict()
@@ -90,12 +91,18 @@ class Apps(commands.Cog):
                     newRole = get(guild.roles, name = 'New Friend')
                     await appMember.add_roles(newRole, reason=f"Approved Application, The user has not played at least one game. I have checked in the last {limit} session-logs")
 
-                await appMember.send(f"Hello, {appMember.name}. Thank you for applying to D&D Friends! One of the moderators on the D&D Friends Mod Team has approved your application and has assigned you the appropriate roles. If you have any further questions please don't hesitate to ask questions in our #help-for-players channel! We hope to see you soon!")
+                await appMember.send(f"Hello, {appMember.name}.\n\nThank you for applying to D&D Friends! The D&D Friends Mod team has approved your application and you have been assigned the appropriate roles.\n\nIf you have any further questions then please don't hesitate to ask in our #help-for-players channel or message a Mod Friend!")
 
-            elif mReaction.emoji == '🚫':
+            elif mReaction.emoji == '❌':
                 await botMsg.edit(embed=None, content=f"{appNum}. {guild.get_member_named(appDiscord).mention} #{appHash} [DENIED]")
                 await botMsg.clear_reactions()
-                await appMember.send(f"Hello, {appMember.name}. Thank you for applying to D&D Friends! Unfortunately the moderators on the D&D Friends Mod team has declined your application since you are not fine explicit/adult content. We hope you find other like-minded people to play D&D with. Good luck!")
+                await appMember.send(f"Hello, {appMember.name}.\n\nThank you for applying to D&D Friends! Unfortunately, the D&D Friends Mod team has declined your application since we do not allow members who are under 18 years of age who are not fine with explicit/adult content (and answered "No" on the application form). If you have any questions or inquiries, please direct them to our Reddit or Twitter accounts:\nReddit - https://www.reddit.com/user/DnDFriends/\nTwitter - https://twitter.com/DnD_Friends\n\nWe hope you find other like-minded people to play D&D with. Good luck!")
+             
+            elif mReaction.emoji = '🚼':
+                await botMsg.edit(embed=None, content=f"{appNum}. {guild.get_member_named(appDiscord).mention} #{appHash} [DENIED - Under 15]")
+                await botMsg.clear_reactions()
+                await appMember.send(f"Hello, {appMember.name}.\n\nUnfortunately, the D&D Friends Mod team has declined your application since we do not allow members who are under 15 years of age. If you have any questions or inquiries, please direct them to our Reddit or Twitter accounts:\nReddit - https://www.reddit.com/user/DnDFriends/\nTwitter - https://twitter.com/DnD_Friends\n\nWe hope you find other like-minded people to play D&D with. Good luck!")
+                
 
 def setup(bot):
     bot.add_cog(Apps(bot))
