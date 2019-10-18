6e31c5c2c4e9d719d2369da2d711d030135cd684
Kevin Vu <kevinvu@Brightfinds-MBP.attlocal.net>
2019-10-18 10:06:43 -0500

    update for multiple logs

--- cogs/logs.py
+++ cogs/logs.py
@@ -23,79 +23,84 @@ class Log(commands.Cog):
         # channel = self.bot.get_channel(577227687962214406) 
         numLogs = int(numLogs)
 
+        if numLogs > 5:
+            break
+
+        def checkLog(oldMsgSplit,msgSplit):
+            patternCP = re.compile(r'([\d.]{0,})(?=CP)\S+(?<=Level)(\d+)[\S?]{0,}\((.*?)\)', re.I)
+            oldlistCP = patternCP.search(oldMsgSplit[2].replace(" ",""))
+            newlistCP = patternCP.search(msgSplit[2].replace(" ",""))
+
+            oldCurrentCP = oldlistCP.group(3).split('/')
+            maxCP = float(oldCurrentCP[1])
+            addCP = float(newlistCP.group(1))
+            currentCP = newlistCP.group(3).split('/')
+
+            tpItemList = msgSplit[3].split(';')
+            print(tpItemList)
+            print(oldMsgSplit[3])
+
+            patternTP = re.compile(r'([\d.]{0,}\s?(?=T))\S+(?<=P).([\w+ ]{0,})\((.*?)\)', re.I)
+            oldlistTP = patternTP.search(oldMsgSplit[3])
+            newlistTP = patternTP.search(tpItemList[0])
+
+            # [1] = CP added, [2] = Current Level, [3] = Current CP / Total CP
+            print(oldlistCP.groups())
+            print(newlistCP.groups())
+            print(oldlistTP.groups())
+            print(newlistTP.groups())
+
+            addTP = float(newlistTP.group(1))
+            tpItem = newlistTP.group(2).strip()
+            oldCurrentTP = oldlistTP.group(3).split('/')
+            currentTP = newlistTP.group(3).split('/')
+            tpCheck = False
+            cpCheck = False
+
+            queryTP = sheet.findall(re.compile(tpItem, re.IGNORECASE))[0]
+            itemMaxTP = int(sheet.cell(3,queryTP.col).value[0:2])
+            refreshKey(refreshTime)
+
+            # TODO check for two items TP
+            if (float(oldCurrentTP[0]) == float(oldCurrentTP[1])):
+                if addTP == float(currentTP[0]):
+                  tpCheck = True
+            else:
+                newCurrentTP = addTP + float(oldCurrentTP[0]) if addTP + float(oldCurrentTP[0]) < itemMaxTP else abs((addTP + float(oldCurrentTP[0])) - itemMaxTP)
+                if newCurrentTP == float(currentTP[0]):
+                    tpCheck = True
+                    
+
+            # TODO: Check for levelup
+            if (float(oldCurrentCP[0]) == float(oldCurrentCP[1])):
+                if addCP == float(currentCP[0]):
+                    cpCheck = True
+            else:
+                newCurrentCP = addCP + float(oldCurrentCP[0]) if addCP + float(oldCurrentCP[0]) < maxCP else abs((addCP + float(oldCurrentCP[0])) - maxCP)
+                if newCurrentCP == float(currentCP[0]):
+                    cpCheck = True
+
+            levelCP = 4 if int(newlistCP.group(2)) < 5 else 8
+
+            print (tpCheck)
+            print (cpCheck)
+
+            return cpCheck and float(currentCP[1]) == levelCP and tpCheck
+
         # TODO check multiple logs
         msgFound = False
         async with channel.typing():
             async for message in channel.history(limit=numLogs, oldest_first=False):
                 msgSplit = message.content.splitlines()
+                print(msgSplit)
                 async for messageOld in channel.history(before=message, oldest_first=False):
                   if msgSplit[0] == messageOld.content.splitlines()[0]:
                       oldMsgSplit = messageOld.content.splitlines()
-                      msgFound = True
+                      if (checkLog(oldMsgSplit,msgSplit)):
+                          await message.add_reaction('✅')
+                      else:
+                          await message.add_reaction('❌')
                       break
-                if msgFound:
-                    break
-
-        patternCP = re.compile(r'([\d.]{0,})(?=CP)\S+(?<=Level)(\d+)\S?\((.*?)\)', re.I)
-        oldlistCP = patternCP.search(oldMsgSplit[2].replace(" ",""))
-        newlistCP = patternCP.search(msgSplit[2].replace(" ",""))
-
-        oldCurrentCP = oldlistCP.group(3).split('/')
-        maxCP = float(oldCurrentCP[1])
-        addCP = float(newlistCP.group(1))
-        currentCP = newlistCP.group(3).split('/')
-
-        tpItemList = msgSplit[3].split(';')
-
-        patternTP = re.compile(r'([\d.]{0,}\s(?=T))\S+(?<=P).([\w+ ]{0,})\((.*?)\)', re.I)
-        oldlistTP = patternTP.search(oldMsgSplit[3])
-        newlistTP = patternTP.search(tpItemList[0])
-
-        # [1] = CP added, [2] = Current Level, [3] = Current CP / Total CP
-        print(oldlistCP.groups())
-        print(newlistCP.groups())
-        print(oldlistTP.groups())
-        print(newlistTP.groups())
-
-        addTP = float(newlistTP.group(1))
-        tpItem = newlistTP.group(2).strip()
-        oldCurrentTP = oldlistTP.group(3).split('/')
-        currentTP = newlistTP.group(3).split('/')
-        tpCheck = False
-        cpCheck = False
-
-        queryTP = sheet.findall(re.compile(tpItem, re.IGNORECASE))[0]
-        itemMaxTP = int(sheet.cell(3,queryTP.col).value[0:2])
-        refreshKey(refreshTime)
-
-        # TODO check for two items TP
-        if (float(oldCurrentTP[0]) == float(oldCurrentTP[1])):
-            if addTP == float(currentTP[0]):
-              tpCheck = True
-        else:
-            newCurrentTP = addTP + float(oldCurrentTP[0]) if addTP + float(oldCurrentTP[0]) < itemMaxTP else abs((addTP + float(oldCurrentTP[0])) - itemMaxTP)
-            if newCurrentTP == float(currentTP[0]):
-                tpCheck = True
-                
-
-        # TODO: Check for levelup
-        if (float(oldCurrentCP[0]) == float(oldCurrentCP[1])):
-            if addCP == float(currentCP[0]):
-                cpCheck = True
-        else:
-            newCurrentCP = addCP + float(oldCurrentCP[0]) if addCP + float(oldCurrentCP[0]) < maxCP else abs((addCP + float(oldCurrentCP[0])) - maxCP)
-            if newCurrentCP == float(currentCP[0]):
-                cpCheck = True
-
-        levelCP = 4 if int(newlistCP.group(2)) < 5 else 8
-
-        print (tpCheck)
-        print (cpCheck)
-
-        if cpCheck and float(currentCP[1]) == levelCP and tpCheck:
-            await message.add_reaction('✅')
-        else:
-            await message.add_reaction('❌')
 
 
 def setup(bot):
