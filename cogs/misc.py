e701b91f0560a387a11d791ea91db0b00877d880
MorizSchildorfer <MorizSchildorfer@users.noreply.github.com>
2020-07-29 20:46:50 -0500

    Corrected IDs

--- cogs/misc.py
+++ cogs/misc.py
@@ -12,8 +12,8 @@ class Misc(commands.Cog, name='Misc'):
         self.current_message= None
         #0: No message search so far, 1: Message searched, but no new message made so far, 2: New message made
         self.past_message_check= 0
-        self.quest_board_channel_id = 728476108940640297 #382027190633627649 728476108940640297
-        self.category_channel_id = 728456686024523810 #382027737189056544  728456686024523810
+        self.quest_board_channel_id = 382027190633627649 #382027190633627649 728476108940640297
+        self.category_channel_id = 382027737189056544 #382027737189056544  728456686024523810
 
 
     #https://discordapp.com/channels/382025597041246210/432358370578530310/733403065251528795 nice comments that make it worth it <3
